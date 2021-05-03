77af6bc88951b1a7684227ae1c87e6eb1eb81477
Robert Meis <rmeis@safesplash.com>
2021-05-03 14:57:40 -0600

    Updates:
    -Fixed brace spacing {} per Google Java style rules.
    -Fixed HTML "<form>" spacing in movie_form.html
    -Added reminder in MovieRepository.java that @Query statement is needed
    -Updated column headers in movie_list.html with suggested titles
    -Added suggestion to use hidden form for date in movie_list.html
    -

--- cst438assignment1/src/main/java/cst438/MovieRepository.java
+++ cst438assignment1/src/main/java/cst438/MovieRepository.java
@@ -3,5 +3,5 @@ package cst438;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MovieRepository extends CrudRepository<Movie, Long> {
-
+	//remember to add JPA query here and create iterable list function.
 }
