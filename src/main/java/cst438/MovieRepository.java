6ef392b28e993aa8d102e583c39f60421c02b58e
jasonco1 <57652308+jasonco1@users.noreply.github.com>
2021-05-03 18:45:26 -0700

    Add files via upload

---
+++ src/main/java/cst438/MovieRepository.java
@@ -0,0 +1,7 @@
+package cst438;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MovieRepository extends CrudRepository<Movie, Long> {
+
+}
---
+++ src/main/java/cst438/Rating.java
@@ -0,0 +1,56 @@
+package cst438;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Rating { // localhost:8080/hello?name=jason
+	
+	@Autowired
+	MovieRepository movieRepository;
+	
+	@GetMapping("/movies")
+	public String hello(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("time", new java.util.Date().toString());
+		return "index";
+		
+	}
+	
+	@GetMapping("/movies/new")
+	public String createPerson(Model model) {
+		Movie movie = new Movie();
+		model.addAttribute("movie", movie);
+		return "movie_form";				
+	}
+	
+	@PostMapping("/movie")
+	public String processMovieForm(@Valid Movie movie, 
+			BindingResult result, 
+			Model model) {
+		if (result.hasErrors()) {
+			return "movie_form";
+		}
+		/* model.addAttribute("date", new java.util.Date().toString()); */
+		movieRepository.save(movie);
+		Iterable<Movie> movies = movieRepository.findAll();
+		model.addAttribute("movies", movies);
+		return "movie_list";
+	}
+	
+	@GetMapping("/movie")
+	public String getAllPerson( Model model) {
+		Iterable<Movie> movies = movieRepository.findAll();
+		model.addAttribute("movies", movies);
+		return "movie_list";	
+	}
+
+
+}
